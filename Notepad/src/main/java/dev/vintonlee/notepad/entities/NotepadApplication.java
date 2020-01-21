3f8b1d65df15b6813ef47d3f00216aea4219794d
i-moonlight <infinite.moonlight0506@gmail.com>
2020-01-20 17:51:16 -0700

    initial

---
+++ Notepad/src/main/java/dev/vintonlee/notepad/entities/NotepadApplication.java
@@ -0,0 +1,13 @@
+package dev.vintonlee.notepad.entities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotepadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotepadApplication.class, args);
+	}
+
+}
---
+++ Notepad/src/test/java/dev/vintonlee/notepad/entities/NotepadApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vintonlee.notepad.entities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotepadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
