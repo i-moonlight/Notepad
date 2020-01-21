9f3bb188bbda0e17901c220078c867e2b31e5041
i-moonlight <infinite.moonlight0506@gmail.com>
2020-01-20 17:56:15 -0700

    updated main and app properties

--- Notepad/src/main/java/dev/vintonlee/notepad/entities/NotepadApplication.java
+++ Notepad/src/main/java/dev/vintonlee/notepad/entities/NotepadApplication.java
@@ -2,10 +2,17 @@ package dev.vintonlee.notepad.entities;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NotepadApplication {
+public class NotepadApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NotepadApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(NotepadApplication.class, args);
 	}
