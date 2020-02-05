e14550bd23be5a1b6e0e4b52c5dcf11740aa9ac3
i-moonlight <infinite.moonlight0506@gmail.com>
2020-02-05 10:12:54 -0700

    fixed typo

--- ngNotepad/src/app/services/notes.service.ts
+++ ngNotepad/src/app/services/notes.service.ts
@@ -52,6 +52,7 @@ export class NotesService {
         'X-Requested-With': 'XMLHttpRequest'
       })
     };
+
     return this.http.get<Note[]>(this.url + username, httpOptions).pipe(
       catchError((err: any) => {
         console.log(err);
