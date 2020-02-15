e18c9ec1f5f49e50b49c248a33a0f12cdb2c790b
i-moonlight <infinite.moonlight0506@gmail.com>
2020-02-15 13:34:40 -0700

    added buttons and title to homepage

--- ngNotepad/src/app/components/home/home.component.ts
+++ ngNotepad/src/app/components/home/home.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authSvc: AuthService) { }
 
   ngOnInit() {
+  }
+
+  userLogInCheck() {
+    return this.authSvc.getCredentials();
   }
 
 }
