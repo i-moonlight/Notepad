4565a6c6a1a203a4ea84c7027a3f85b1370a1047
i-moonlight <infinite.moonlight0506@gmail.com>
2020-02-15 12:34:01 -0700

    user can register

--- ngNotepad/src/app/components/register/register.component.ts
+++ ngNotepad/src/app/components/register/register.component.ts
@@ -1,4 +1,9 @@
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { AuthService } from './../../services/auth.service';
+import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  newUser: User;
 
-  constructor() { }
+  constructor(private authSvc: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  register(registerForm: NgForm) {
+    this.newUser = registerForm.value;
+
+    this.authSvc.register(this.newUser).subscribe(
+      success => {
+        this.authSvc.login(this.newUser.username, this.newUser.password).subscribe(
+          login => {
+            this.router.navigateByUrl('/notes');
+          },
+          failed => { this.router.navigateByUrl('/login'); }
+        );
+      },
+      failure => {
+        this.router.navigateByUrl('/register');
+      }
+    );
+
+  }
 }
