59977591b4faffdf749bf77873a13914c78eae5c
i-moonlight <infinite.moonlight0506@gmail.com>
2020-02-15 14:06:46 -0700

    can see user data in profile

--- ngNotepad/src/app/components/profile/profile.component.ts
+++ ngNotepad/src/app/components/profile/profile.component.ts
@@ -1,4 +1,7 @@
+import { Router } from '@angular/router';
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  user: User = new User();
 
-  constructor() { }
+  constructor(private authSvc: AuthService, private router: Router) { }
 
   ngOnInit() {
+    if (!this.authSvc.getCredentials()) {
+      this.router.navigateByUrl('/home');
+    }
+
+    this.loadUser();
+  }
+
+  loadUser() {
+    this.authSvc.getUserByUsername(this.authSvc.getLoggedInUsername()).subscribe(
+      success => {
+        this.user = success;
+        console.log(this.user.username);
+
+      },
+      failure => { }
+    );
   }
 
 }
