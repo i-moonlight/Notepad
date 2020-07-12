50fad3ed5d5b874414f5596175a2c6f476c23f74
i-moonlight <infinite.moonlight0506@gmail.com>
2020-07-12 14:11:25 -0600

    improved performance, added final

--- Notepad/src/main/java/dev/vintonlee/notepad/security/SecurityConfig.java
+++ Notepad/src/main/java/dev/vintonlee/notepad/security/SecurityConfig.java
@@ -16,43 +16,32 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    // this you get for free when you configure the db connection in application.properties file
-    @Autowired
-    private DataSource dataSource;
+	// this you get for free when you configure the db connection in
+	// application.properties file
+	@Autowired
+	private DataSource dataSource;
 
-    // this bean is created in the application starter class if you're looking for it
-    @Autowired
-    private PasswordEncoder encoder;
+	// this bean is created in the application starter class if you're looking for
+	// it
+	@Autowired
+	private PasswordEncoder encoder;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-        .csrf().disable()
-        .authorizeRequests()
-        .antMatchers(HttpMethod.OPTIONS, "/api/**").permitAll() // For CORS, the preflight request
-        .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()     // will hit the OPTIONS on the route
-        
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/api/**").permitAll()
 
-        
-        .antMatchers("/api/**").authenticated() // Requests for our REST API must be authorized.
-        .anyRequest().permitAll()               // All other requests are allowed without authorization.
-        .and()
-        .httpBasic();                           // Use HTTP Basic Authentication
+				.antMatchers("/api/**").authenticated() // Requests for our REST API must be authorized.
+				.anyRequest().permitAll() // All other requests are allowed without authorization.
+				.and().httpBasic(); // Use HTTP Basic Authentication
 
-        http
-        .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        String userQuery = "SELECT username, password, enabled FROM User WHERE username=?";
-        String authQuery = "SELECT username, role FROM User WHERE username=?";
-        auth
-        .jdbcAuthentication()
-        .dataSource(dataSource)
-        .usersByUsernameQuery(userQuery)
-        .authoritiesByUsernameQuery(authQuery)
-        .passwordEncoder(encoder);
-    }
+	@Override
+	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+		final String userQuery = "SELECT username, password, enabled FROM User WHERE username=?";
+		final String authQuery = "SELECT username, role FROM User WHERE username=?";
+		auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery(userQuery)
+				.authoritiesByUsernameQuery(authQuery).passwordEncoder(encoder);
+	}
 }
